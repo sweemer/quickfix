1581ab7ef4f60d4c1d8b933b313561ed4cb15a79
Jonathan Sweemer <sweemer@gmail.com>
2026-05-22 06:06:22 +0000

    Migrate CMake tests to CTest

--- test/runat.bat
+++ test/runat.bat
@@ -2,8 +2,8 @@ echo off
 if "%1" == "" goto usage
 if "%2" == "" goto usage
 
-if "%1" == "debug" goto debug:
-if "%1" == "release" goto release:
+if /I "%1" == "debug" goto debug:
+if /I "%1" == "release" goto release:
 goto usage
 
 :debug
